780b2b8014c9130343ff55150fc4ab603428bea0
Fatzel D-Almeida <fatzel.d-almeida@epita.fr>
2022-02-11 19:19:45 +0100

    Part 1

---
+++ src/Launcher.java
@@ -0,0 +1,17 @@
+import java.util.scanner
+Public class Launcher {
+    Public static void main(String[] args) {
+        system.out.println("Welcome !");
+        Scanner a = new Scanner(System.in);
+        if (a == 'quit')
+        {
+            break;
+        }
+
+        else
+        {
+            system.out.println("Unknown Command");
+            break;
+        }
+    }
+}
